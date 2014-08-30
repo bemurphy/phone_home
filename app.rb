fac7ada00177f631213b0a07f32f9e4c764e4f77
Brendon Murphy <xternal1+github@gmail.com>
2014-08-30 12:36:22 -0700

    Store locations in a hash instead.
    This makes it simple to return all locations without resorting
    to an index

--- app.rb
+++ app.rb
@@ -1,21 +1,32 @@
-require "cuba"
 require "basica"
+require "cuba"
+require "json"
 require "redic"
 
 Cuba.plugin Basica
 
 class IpTracking
+  KEY = "locations"
+
   def self.redis
     url = ENV['REDIS_URL'] || ENV['REDISTOGO_URL'] || "redis://localhost:6379/"
     @redis ||= Redic.new(url)
   end
 
   def self.set(location, value)
-    redis.call('SET', "ip:#{location}", value)
+    redis.call('HSET', KEY, location, value)
   end
 
   def self.get(location)
-    redis.call('GET', "ip:#{location}")
+    redis.call('HGET', KEY, location)
+  end
+
+  def self.del(location)
+    redis.call('HDEL', KEY, location)
+  end
+
+  def self.all
+    Hash[*redis.call('HGETALL', KEY)]
   end
 end
 
@@ -31,27 +42,39 @@ Cuba.define do
   end
 
   authenticated =  basic_auth(env) do |_, pass|
-    Rack::Utils.secure_compare(pass, ENV.fetch('PASSWORD'))
+    Rack::Utils.secure_compare(pass.to_s, ENV.fetch('PASSWORD'))
   end
 
-  on authenticated, "track/:location" do |location|
-    res.headers['Content-Type'] = 'text/plain'
-
-    on post do
-      IpTracking.set(location, req.ip)
-      res.write 'ok'
+  on authenticated, "track" do
+    on get, root do
+      res.headers["Content-Type"] = "application/json; charset=utf-8"
+      res.write IpTracking.all.to_json
     end
 
-    on get do
-      location_ip = IpTracking.get(location)
+    on ":location" do |location|
+      res.headers['Content-Type'] = 'text/plain'
 
-      on !location_ip.to_s.empty? do
-        res.write location_ip
+      on post do
+        IpTracking.set(location, req.ip)
+        res.write 'ok'
       end
 
-      on default do
-        res.status = 404
-        res.write 'Not Found'
+      on delete do
+        IpTracking.del(location)
+        res.write 'ok'
+      end
+
+      on get do
+        location_ip = IpTracking.get(location)
+
+        on !location_ip.to_s.empty? do
+          res.write location_ip
+        end
+
+        on default do
+          res.status = 404
+          res.write 'Not Found'
+        end
       end
     end
   end
